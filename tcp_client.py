82aa26a49a736159e8f3b666209601827e7d5e8d
Mathias Neerup <manee12@student.sdu.dk>
2017-09-25 10:49:46 +0200

    Code used to start measurements (trigger) on camera

---
+++ tcp_client.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+
+import socket
+
+
+TCP_IP = '192.168.1.10'
+TCP_PORT = 9876
+BUFFER_SIZE = 1024
+MESSAGE = "MEASURE /C\r\n"
+
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.connect((TCP_IP, TCP_PORT))
+s.send(MESSAGE)
+data = s.recv(BUFFER_SIZE)
+s.close()
+
+print "received data:", data
+
